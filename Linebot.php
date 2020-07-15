ac04f2047f4d8d922bce4842b81b97801e24c57d
phong9479@gmail.com <phongnarin@CSIGROUPS.COM>
2020-07-15 16:57:26 +0700

    test

--- Linebot.php
+++ Linebot.php
@@ -34,7 +34,7 @@ if ( sizeof($request_array['events']) > 0 ) {
       $text = $event['message']['text'];
       $data = [
          'replyToken' => $reply_token,
-         'messages' => [['type' => 'text', 'text' => "Phong" ]]
+         'messages' => [['type' => 'text', 'text' => "$text" ]]
       ];
       $post_body = json_encode($data, JSON_UNESCAPED_UNICODE);
       $send_result = send_reply_message($API_URL.'/reply',      $POST_HEADER, $post_body);
